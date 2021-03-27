2f25cf6cec2ac52ead6570ecfd45d2a3106b6963
MomohNobert <okhai0112@gmail.com>
2021-03-27 23:49:06 +0100

    changes to index structure

--- innoloft-dashboard/src/app/index.js
+++ innoloft-dashboard/src/app/index.js
@@ -1,8 +1,26 @@
+import styled from 'styled-components';
+import Header from '../shared/Header/header.component';
+import {GlobalStyle} from '../styles/GlobalStyle';
+
+const HeaderStyle = styled.header`
+  height: 55px;
+  width: 100%;
+  position: fixed;
+  box-shadow: 0px 0px 3px rgba(0,0,0,0.5);
+`
+
+const Content = styled.div`
+  margin-top: 75px;
+`
+
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <GlobalStyle />
+      <HeaderStyle>
+        <Header />
+      </HeaderStyle>
+    </>
   );
 }
 
