1809576fc79709b4afddb66b63d3c40417c6d848
MomohNobert <okhai0112@gmail.com>
2021-03-27 23:23:14 +0100

    adding dependencies

---
+++ innoloft-dashboard/src/styles/GlobalStyle.js
@@ -0,0 +1,46 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    border: 0;
+    outline: none;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+    outline: none;
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    -webkit-tap-highlight-color: transparent;
+  }
+
+  body {
+    background-color: white;
+    width: 100vw;
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  ::-webkit-scrollbar {
+    width: 5px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background: black;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: white;
+    border-radius: 10%;
+  }
+
+  ::-webkit-scrollbar-thumb:hover {
+    background: blue;
+  }
+`;
