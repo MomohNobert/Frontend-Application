891e7cc966edb139f27fca6b07f612ae1cbf82bc
MomohNobert <okhai0112@gmail.com>
2021-03-28 01:37:37 +0100

    created product main infp component

---
+++ innoloft-dashboard/src/app/components/ProductMainInfo/pmi.component.jsx
@@ -0,0 +1,16 @@
+import { PMIStyle } from "./pmi.styles"
+
+function PMI({ name, investmentEffort, type }) {
+    console.log(name, investmentEffort, type)
+    return (
+        <PMIStyle>
+            <div className="title">
+                <h1>{name}</h1>
+                <span>({type.name})</span>
+            </div>
+            <h3>{investmentEffort}</h3>
+        </PMIStyle>
+    )
+}
+
+export default PMI
---
+++ innoloft-dashboard/src/app/components/ProductMainInfo/pmi.styles.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const PMIStyle = styled.div`
+    width: 100%;
+    height: 100px;
+    margin-top: 15px;
+    display: flex;
+    align-items: center;
+
+    .title {
+        display: flex;
+        flex-direction: column;
+        margin-right: 50px;
+
+        span {
+            font-size: 15px;
+        }
+    }
+
+    h3 {
+        color: green;
+    }
+`
