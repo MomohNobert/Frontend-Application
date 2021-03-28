c708c9aaf0a60e63b15113ba3a543eadf51ecb37
MomohNobert <okhai0112@gmail.com>
2021-03-28 12:03:49 +0100

    changed attribute component

--- innoloft-dashboard/src/app/components/Attributes/attributes.component.jsx
+++ innoloft-dashboard/src/app/components/Attributes/attributes.component.jsx
@@ -1,5 +1,16 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchTrl, trlSelector } from "./attributes.slice";
+
 function Attributes() {
-  return <p>Attributes</p>;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchTrl())
+  }, [dispatch])
+
+  const { loading, hasErrors, trl } = useSelector(trlSelector);
+  console.log(loading, hasErrors, trl)
+  return loading && <p>Hello</p>;
 }
 
 export default Attributes;
