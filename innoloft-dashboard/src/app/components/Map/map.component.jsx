14aea30ec4d625302f2f9785facb177caf3373a6
MomohNobert <okhai0112@gmail.com>
2021-03-28 03:42:37 +0100

    modified map component

--- innoloft-dashboard/src/app/components/Map/map.component.jsx
+++ innoloft-dashboard/src/app/components/Map/map.component.jsx
@@ -1,11 +1,32 @@
 import { MStyle } from "./map.styles";
+import GoogleMapReact from 'google-map-react'
 
-function Map() {
+function Map({ address }) {
+    const { latitude, longitude } = address
+    const props = {
+        center : {
+            lat: Math.round(latitude * 1000) / 1000,
+            lng: Math.round(longitude * 1000) / 1000
+        },
+        zoom: 15
+    }
     return (
-        <MStyle>
-
+        <MStyle style={{ height: "275px", width: "100%"}}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API_KEY}}
+                defaultCenter={props.center}
+                defaultZoom={props.zoom}
+            >
+                <AnyReactComponent
+                    lat={latitude}
+                    lng={longitude}
+                    text="My Marker"
+                />
+            </GoogleMapReact>
         </MStyle>
     )
 }
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 export default Map
