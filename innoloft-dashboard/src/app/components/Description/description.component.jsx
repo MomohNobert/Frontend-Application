0de49b01b2fbee6f2dc1cd2b1215906bf3c27d42
MomohNobert <okhai0112@gmail.com>
2021-03-28 03:42:48 +0100

    modified description component

--- innoloft-dashboard/src/app/components/Description/description.component.jsx
+++ innoloft-dashboard/src/app/components/Description/description.component.jsx
@@ -1,5 +1,6 @@
-function Description() {
-  return <p>Description</p>;
+function Description({ description }) {
+  const desc = {__html: description}
+  return <p dangerouslySetInnerHTML={desc} />;
 }
 
 export default Description;
