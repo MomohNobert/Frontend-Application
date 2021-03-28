2f7cb3a14838a28284c3e35f6439ab2885b7d8ee
MomohNobert <okhai0112@gmail.com>
2021-03-28 02:29:37 +0100

    created userInfo component

---
+++ innoloft-dashboard/src/app/components/UserInfo/userInfo.component.jsx
@@ -0,0 +1,11 @@
+import { UIStyle } from "./userInfo.styles"
+
+function UserInfo() {
+    return (
+        <UIStyle>
+            <p>UserInfo</p>
+        </UIStyle>
+    )
+}
+
+export default UserInfo
---
+++ innoloft-dashboard/src/app/components/UserInfo/userInfo.styles.jsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const UIStyle = styled.div`
+    width: 100%;
+    height: 350px;
+    box-shadow: 0px 0px 3px rgba(0,0,0,0.3);
+`
